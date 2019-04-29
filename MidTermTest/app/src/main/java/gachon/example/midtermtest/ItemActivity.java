c02e3950a3e3b35b6265e03349e42a53f68dd26e
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2019-04-29 19:06:57 +0900

    MidTermTest
    Fuck You docent

---
+++ MidTermTest/app/src/main/java/gachon/example/midtermtest/ItemActivity.java
@@ -0,0 +1,38 @@
+package gachon.example.midtermtest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ItemActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private EditText edit_text;
+    private Button bt_ok;
+    private Intent i;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+        i=getIntent();
+        edit_text=(EditText)findViewById(R.id.edit);
+        bt_ok=(Button)findViewById(R.id.bt_ok);
+        bt_ok.setOnClickListener(this);
+
+        edit_text.setText(getIntent().getStringExtra("string"));
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        String edit_string=edit_text.getText().toString();
+        i.putExtra("edit_string",edit_string);
+        setResult(RESULT_OK,i);
+        finish();
+    }
+}
