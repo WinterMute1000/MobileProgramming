22aade796fe91f02f6a9ec2cde0fe2eb9b99fed3
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2019-05-25 03:33:05 +0900

    First
    Not finished

---
+++ TimesTableGame/app/src/main/java/gachon/example/timestablegame/MainActivity.java
@@ -0,0 +1,70 @@
+package gachon.example.timestablegame;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    ListView resultView;
+    ArrayList<String> result=new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    static final int MENU_GAME_START=1;
+    static final int TIMES_TABLE_GAME=1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultView=(ListView)findViewById(R.id.game_result);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                result);
+        resultView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case MENU_GAME_START:
+                Intent i=new Intent(this,TimesTableActivity.class);
+                startActivityForResult(i,TIMES_TABLE_GAME);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        menu.add(Menu.NONE,MENU_GAME_START,Menu.NONE,
+                "Game Start");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        if(requestCode==TIMES_TABLE_GAME)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                String game_result = data.getStringExtra("gameResult");
+                result.add(game_result);
+                adapter.notifyDataSetChanged();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ TimesTableGame/app/src/main/java/gachon/example/timestablegame/TimesTableActivity.java
@@ -0,0 +1,15 @@
+package gachon.example.timestablegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimesTableActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_times_table);
+    }
+}
